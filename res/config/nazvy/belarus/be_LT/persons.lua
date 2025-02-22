d1a810683702529300571c140864682adc93f14a
Aliaksei Kapura <aliaksei.kapura@any-ware.nl>
2025-02-22 14:05:37 +0300

    Пераклад місіі "Вызваленыя рынкі", агульны поступ 72,93%. Aŭtamazavanaje pieraŭtvareńnie tekstu da łacinki

---
+++ res/config/nazvy/belarus/be_LT/persons.lua
@@ -0,0 +1,97 @@
+local names = { }
+
+names.belarus = {
+    belarusian = {
+        firstNamesMale = {
+					"Adam", "Akim", "Aleh", "Alaksandar", "Alaksiej", "Anatol", "Andrej", "Anisim", "Anton", "Arkadź", "Arnold", "Arsienij", "Artur", "Arciom", "Astap", "Afanasij",
+					"Bahdan", "Bahusłaŭ", "Bazyl", "Balasłaŭ", "Banifacyj", "Barys", "Barysłaŭ", "Budysłaŭ", "Branisłaŭ", "Bračysłaŭ", "Budzimir",
+					"Vadzim", "Valeryj", "Valancin", "Varfałamiej", "Vasil", "Vieniadzikt", "Vincent", "Viktar", "Vitalij", "Vitaŭt", "Viačasłaŭ",
+					"Harysłaŭ", "Haŭryła", "Hieorhij", "Hierasim", "Hianadź", "Hleb",
+					"Davyd", "Daminik", "Daniła", "Darafiej", "Dźmitryj", "Dzianis",
+					"Ždan",
+					"Zachar",
+					"Ihar", "Ihnat", "Illa", "Ivan",
+					"Iozef",
+					"Kazimir", "Karł", "Kirył", "Kandrat", "Kanstancin", "Kuźma",
+					"Łaŭrenci", "Łieanid",
+					"Maksim", "Matviej", "Miron", "Michaił", "Mikita", "Mikałaj",
+					"Nikifar",
+					"Pavieł", "Piotra", "Prakofij",
+					"Radzivon", "Raman", "Ryhor",
+					"Śviatasłaŭ", "Stanisłaŭ", "Ściapan", "Siamion", "Symon", "Siarhiej",
+					"Taras", "Trafim",
+					"Uładzimier", "Uładzisłaŭ",
+					"Fiodar", "Filip", "Francysk",
+					"Charłam", "Chryściian",
+					"Cimafiej", "Cichan",
+					"Časłaŭ",
+					"Eduard", "Eŭdakim", "Emiallan",
+					"Ulij", "Uryj",
+					"Ahor", "Akaŭ", "An", "Aŭhen", "Afim"
+        },
+        firstNamesFemale = {
+            "Ahapiia", "Ahrypina", "Adełaida", "Akulina", "Ała", "Alena", "Alesia", "Alina", "Alisa", "Alaksandara", "Anastasyia", "Anhielina", "Anžalika", "Anžeła", "Antanina", "Antoniia", "Anna", "Aŭdoćcia",
+						"Barbara", "Budysłava", "Branisłava", "Beła",
+						"Valeryia", "Valancina", "Vanda", "Vera", "Veranika", "Viktoryia", "Vitalina", "Viialeta", "Volha", "Venera", "Viačasłava",
+						"Hanna", "Halina",
+						"Daminika", "Darafieia", "Dar’ia", "Žana",
+						"Zynaida", "Zosia",
+						"Irena", "Iryna",
+						"Karalina", "Kryścina", "Ksieniia", "Kaciaryna",
+						"Łarysa", "Lidziia", "Lizavieta", "Liliia", "Łiubava", "Łiubamira", "Łiuboŭ", "Łiudmiła", "Łiucyia",
+						"Mahdalena", "Marharyta", "Maryia", "Maryna",
+						"Nadzieia", "Natalla", "Nina", 
+						"Palina",
+						"Raisa", "Rehina",
+						"Śviatłana", "Sof’ia",
+						"Taisiia", "Tamara", "Taćciana",
+						"Ullana",
+						"Fiokła", "Fiała",
+						"Chryściiana", "Chryścina",
+						"Časłava",
+						"Eva", "Elvira", "Elina", "Ema", "Emiliia", "Eŭdakiia", "Eŭrasińnia",
+						"Ulla", "Uzefa",
+						"Adviha", "Aŭheniia", "Ana"
+				},
+        lastNames = {
+					"Ivanoŭ", "Siarhieieŭ", "Mikałaieŭ", "Armolenka", "Dubovik", "Kavaloŭ", "Sokałaŭ", "Paŭłoŭski",	"Šaŭčuk",	"Vajtovič",
+					"Karpienka", "Staravojtaŭ", "Piatrovič", "Łapacin", "Hurko", "Astapčuk", "Kraviec", "Zaleski", "Łiebiedzieŭ", "Palakovič",
+					"Hrynievič", "Sivicki", "Cybulski", "Chrapko", "Šyško", "Acko", "Kraŭčanka", "Ramanoŭski", "Niačaj", "Łiaŭkovič", "Zubko",
+					"Martyniuk", "Fiodaraŭ", "Baranaŭ", "Karatkievič", "Žukoŭski", "Radzivił", "Dziamidovič", "Čarkasaŭ", "Bialaieŭ", "Kaściučenka",
+					"Pałonski", "Patapovič", "Łahviniec", "Višnieŭski", "Bahdanovič", "Klimkovič", "Čarniaŭski", "Burak", "Zubar", "Kuprejčyk",
+					"Kazłoŭ", "Michałkievič", "Aroš", "Płatonaŭ", "Šukieła", "Hołub", "Łohvinaŭ", "Martynovič", "Aleksandrovič", "Asinski",
+					"Rudnik", "Tur", "Šklar", "Daškievič", "Mielnik", "Hryb", "Akubovič", "Nikałaienka", "Ažhirej", "Žukoŭ", "Sasnoŭski",
+					"Bandarenka", "Baranoŭski", "Vasiučenka", "Vołkaŭ", "Dudka", "Zahorski", "Kisialoŭ", "Kučynski", "Łiasnoŭ", "Marcinkievič",
+					"Mileŭski", "Piatkievič", "Ramančyk", "Savicki", "Sacharčuk", "Sviryd", "Sidarovič", "Silčanka", "Słabadzianiuk", 
+					"Staraževič", "Tarasievič", "Tkačenka", "Tupica", "Famičoŭ", "Chomič", "Cichanovič", "Čarnoŭ", "Šaviałoŭ", "Ankoŭski"
+        }
+    }
+}
+
+local firstNamesMale = names.belarus.belarusian.firstNamesMale
+local firstNamesFemale = names.belarus.belarusian.firstNamesFemale
+local lastNames = names.belarus.belarusian.lastNames
+
+function data()
+    return {
+        makeName = function(male)
+            if (male) then
+                return firstNamesMale[math.random(#firstNamesMale)] .. " " .. lastNames[math.random(#lastNames)]
+            else
+                return firstNamesFemale[math.random(#firstNamesFemale)] .. " " .. lastNames[math.random(#lastNames)]
+            end
+        end
+    }
+end
+
+function data()
+    return {
+        makeName = function(male)
+            if (male) then
+                return firstNamesMale[math.random(#firstNamesMale)] .. " " .. lastNames[math.random(#lastNames)]
+            else
+                return firstNamesFemale[math.random(#firstNamesFemale)] .. " " .. lastNames[math.random(#lastNames)]
+            end
+        end
+    }
+end
