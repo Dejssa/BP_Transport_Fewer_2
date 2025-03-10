6e207d7e477c11aaba89f5e9bf519831d6ee6efa
Aliaksei Kapura <aliaksei.kapura@any-ware.nl>
2025-03-10 12:50:49 +0300

    Пераклад місіі "Лідар"

--- mod.lua
+++ mod.lua
@@ -5,7 +5,7 @@ function data()
             severityAdd = "NONE",
             severityRemove = "NONE",
             name = _("Беларускі пераклад"),
-            description = "Аматарскі пераклад на беларускую (клясічны).\n\nКірыліца:\n- Каб ужыць пераклад, перайдзіце да налад гульні й выберыце ў мовах \"Беларуская (Mod)\"\n- Каб ужыць беларускія назвы местаў/вуліцаў/людзей, пад час стварэньня гульні або запуску існуючай уключыце мод. \"Беларускі пераклад\", далей у Пашыраных наладах для \"Назвы местаў\" выберыце \"Беларускі пераклад\"\n\nŁacinka:\n- Kab užyć pierakład, pierajdzicie da naład hulni j vybierycie ŭ movach ”Biełaruskaja (Mod)”\n- Kab užyć biełaruskija nazvy miestaŭ/vulicaŭ/ludziej, pad čas stvareńnia hulni abo zapusku isnujučaj uklučycie mod. ”Biełaruski pierakład”, dalej u Pašyranych naładach dla ”Nazvy miestaŭ” vybierycie ”Biełaruski pierakład”",
+            description = "Аматарскі пераклад на беларускую (клясічны).\n\nКірыліца:\n- Каб ужыць пераклад, перайдзіце да налад гульні й выберыце ў мовах \"Беларуская (Mod)\"\n- Каб ужыць беларускія назвы местаў/вуліцаў/людзей, пад час стварэньня гульні або запуску існуючай уключыце мод. \"Беларускі пераклад\", далей у Пашыраных наладах для \"Назвы местаў\" выберыце \"Беларускі пераклад\"\n\nŁacinka:\n- Kab užyć pierakład, pierajdzicie da naład hulni j vybierycie ŭ movach ”Biełaruskaja (Mod)”\n- Kab užyć biełaruskija nazvy miestaŭ/vulicaŭ/ludziej, pad čas stvareńnia hulni ci zapusku isnujučaj uklučycie mod. ”Biełaruski pierakład”, dalej u Pašyranych naładach dla ”Nazvy miestaŭ” vybierycie ”Biełaruski pierakład”",
             tags = { "Localization", "Belarusian" },
             authors = {
                 {
